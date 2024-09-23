b206d5bcf9c7c3444007ba84db38308c0e492854
artur tenca <arturtenca1@gmail.com>
2024-09-23 09:02:33 -0300

    nav style

--- src/css/NavStyle.jsx
+++ src/css/NavStyle.jsx
@@ -1,6 +1,30 @@
 import styled from "styled-components";
 
 export const NavStyle = styled.section`
-
-
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+    list-style:none;
+    text-decoration:none;
+}
+section{
+    background-color:#646464;
+}
+section nav{
+    text-align: center;
+    padding-top: 5px;
+    padding-bottom: 5px;
+}
+section nav a{
+    font-size: 20px;
+    text-decoration: none;
+    color: black;
+    margin-right: 10px;
+    transition:all 0.7s;
+}
+section nav a:hover{
+   font-size: 22px;
+   
+}
 `
