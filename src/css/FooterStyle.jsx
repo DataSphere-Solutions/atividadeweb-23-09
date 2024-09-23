e94d939d42b4183e2e2cb4957daf831b6bb82421
artur tenca <arturtenca1@gmail.com>
2024-09-23 09:13:32 -0300

    fazndeo style do login

--- src/css/FooterStyle.jsx
+++ src/css/FooterStyle.jsx
@@ -6,5 +6,12 @@ padding: 0;
 display: flex;
 justify-content: center;
 
+    h5{
+        text-align: center;
+        background-color: #999999;
+        padding-top: 20px;
+        padding-bottom: 20px;
+        width: 100%;
+    }
 
 `
